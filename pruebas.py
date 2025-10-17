dc5869a4df979dea996c16009278fde19d31c91c
Antony Alcalá <83379350+antony2404@users.noreply.github.com>
2025-10-17 11:45:43 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,8 @@
 from principal import calcular_total
 
-def test_calcular_total():
+def pruebas_calcular_total():
     assert calcular_total([100, 200, 300]) == 600
     assert calcular_total([]) == 0
 
     assert calcular_total([50]) == 50
+
