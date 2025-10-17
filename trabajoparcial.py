43de8eccc04e970049eadfbb5321e243c592c3c2
Antony Alcalá <83379350+antony2404@users.noreply.github.com>
2025-10-17 11:29:38 -0500

    Update trabajoparcial.py

--- trabajoparcial.py
+++ trabajoparcial.py
@@ -1,7 +1,8 @@
 def calcular_total(ventas):
     """Calcula el total de ventas dadas en una lista."""
     return sum(ventas)
+if __name__ == "principal":
+    ventas = [100, 250, 75, 300]
+    total = calcular_total(ventas)
 
-ventas = [100, 250, 75, 300]
-total = calcular_total(ventas)
-print(f"Total de ventas del día: ${total}")
+    print(f"Total de ventas del día: ${total}")
