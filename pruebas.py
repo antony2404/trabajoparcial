05adf3f3bc485e38c0504eafe00dc139b7d6be4d
Antony Alcalá <83379350+antony2404@users.noreply.github.com>
2025-10-17 11:30:22 -0500

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,6 @@
+from __main__ import calcular_total
+
+def test_calcular_total():
+    assert calcular_total([100, 200, 300]) == 600
+    assert calcular_total([]) == 0
+    assert calcular_total([50]) == 50
